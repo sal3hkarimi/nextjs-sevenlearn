dea74a371600c2ba88c3546aa1eeff0c459d4cde
Azim Karimi <azimkarimi315@gmail.com>
2023-03-11 17:14:24 +0330

    unit-14 Link

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,15 +4,28 @@
 // import styles from '@/styles/Home.module.css'
 // const inter = Inter({ subsets: ['latin'] })
 
-import {loadPosts} from '../lib/posts'
+import { loadPosts } from '../lib/posts'
+import Link from 'next/link'
 
 type homeProps = {
   posts?: any
   post?: any
 }
 
-export default function Home({ posts }:homeProps) {
-  const postTitle = posts.map((post:any) => <p key={post.id}>{post.title}</p>)
+export default function Home({ posts }: homeProps) {
+  const postTitle = posts ? posts.map((post: any) => (
+    <Link 
+    prefetch={true}
+    href={{
+      pathname: 'posts/[slug]',
+      query: {
+        slug: post.slug,
+        foo: 'fooo'
+      }
+    }}>
+      <p key={post.id}>{post.title}</p>
+    </Link>
+  )) : <p>No Post</p>
 
   return (
     <>
@@ -25,7 +38,7 @@ export default function Home({ posts }:homeProps) {
 
 
 export async function getStaticProps() {
-  const posts = await loadPosts()  
+  const posts = await loadPosts()
 
   return {
     props: {
