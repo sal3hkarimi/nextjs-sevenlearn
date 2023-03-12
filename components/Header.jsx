8134cae7520fefef07311fdee65568f8f2c5e82f
Azim Karimi <azimkarimi315@gmail.com>
2023-03-12 13:27:50 +0330

    unit-17 style

---
+++ components/Header.jsx
@@ -0,0 +1,8 @@
+import {style} from './Footer.module.css'
+
+export default function Header({ title }) {
+
+    return (
+        <header>This is header website</header>
+    )
+}
---
+++ pages/sty/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Body from '../../components/Body'
+import Footer from '../../components/Footer'
+import Header from '../../components/Header'
+import {style} from './index.module.css'
+
+export default function Style() {
+    console.log(style.name);
+    return (
+        <>
+            <Header />
+            <Body />
+            <Footer />
+            {/* <style jsx global>
+                {
+                    `
+                    body{
+                        background: red
+                    }
+                    `
+                }
+            </style> */}
+        </>
+    )
+}
